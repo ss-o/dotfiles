d42557b0db2c2d01e5e17ceb4e5d531cca0239c2
Salvydas Lukosius <team@zshell.dev>
2022-07-19 00:32:41 +0100

    Update z-shell-org.zsh

--- .codeload/_local---config/zsh/aliases/z-shell-org.zsh
+++ .codeload/_local---config/zsh/aliases/z-shell-org.zsh
@@ -1,4 +1,6 @@
 # CONFIG FILES
-# Fetch default repository files.
-alias add-gitattributes="command wget -O - https://raw.githubusercontent.com/alexkaratarakis/gitattributes/master/Common.gitattributes >> .gitattributes"
-alias add-editorconfig="command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/editor/editorconfig/.editorconfig-root > .editorconfig"
+#
+# Fetch common repository files.
+alias add-gitattributes='command wget -O - https://raw.githubusercontent.com/alexkaratarakis/gitattributes/master/Common.gitattributes >> .gitattributes'
+alias add-editorconfig='command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/editor/editorconfig/.editorconfig-root > .editorconfig'
+alias add-gitignore='command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/git/gitignore_common >> .gitignore'
