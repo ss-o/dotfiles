5e4bbebce6636d13c57aeb85f1556b658a4d4537
Salvydas Lukosius <team@zshell.dev>
2022-07-18 19:33:37 +0100

    Create z-shell-org.zsh

---
+++ .codeload/_local---config/zsh/aliases/z-shell-org.zsh
@@ -0,0 +1,4 @@
+# CONFIG FILES
+# Fetch default repository files.
+alias add-gitattributes="command wget -O - https://raw.githubusercontent.com/alexkaratarakis/gitattributes/master/Common.gitattributes >> .gitattributes"
+alias add-editorconfig="command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/editor/editorconfig/.editorconfig-root > .editorconfig"
