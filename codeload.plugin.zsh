d9403d31bffef63ca3deceeffa0d0d261a325905
Salvydas Lukosius <team@zshell.dev>
2022-07-18 16:55:15 +0100

    Update codeload.plugin.zsh

--- codeload.plugin.zsh
+++ codeload.plugin.zsh
@@ -1,19 +1,21 @@
 # -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
 # vim: ft=zsh sw=2 ts=2 et
 #
+# https://z.digitalclouds.dev/community/zsh_plugin_standard#standard-recommended-options
+builtin emulate -L zsh ${=${options[xtrace]:#off}:+-o xtrace}
+builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc_quotes no_auto_pushd
+
 # https://z.digitalclouds.dev/community/zsh_plugin_standard#zero-handling
 0="${ZERO:-${${0:#$ZSH_ARGZERO}:-${(%):-%N}}}"
 0="${${(M)0:#/*}:-$PWD/$0}"
 
 # https://z.digitalclouds.dev/community/zsh_plugin_standard#standard-plugins-hash
 typeset -gA Plugins
+typeset -A ZState
 Plugins[CODELOAD_REPO_DIR]="${0:h}"
 
-# https://z.digitalclouds.dev/community/zsh_plugin_standard#preventing-parameter-pollution
-typeset -A ZState
-
 autoload_func() {
-  fpath=("${0:h}/functions" "${fpath[@]}")
+  fpath=("${Plugins[CODELOAD_REPO_DIR]}/functions" "${fpath[@]}")
   autoload -Uz $fpath[1]/*(.:t)
   ZState[autoload_func]=1
 }
