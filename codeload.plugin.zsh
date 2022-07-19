f07953b0c3b7e1cb91fe5ccfae2d2828a81f9372
Salvydas Lukosius <team@zshell.dev>
2022-07-19 21:07:01 +0100

    Update codeload.plugin.zsh

--- codeload.plugin.zsh
+++ codeload.plugin.zsh
@@ -13,7 +13,6 @@ builtin setopt extended_glob warn_create_global typeset_silent no_short_loops rc
 typeset -gA ZI Plugins ZState
 Plugins[CODELOAD_REPO_DIR]="${0:h}"
 ZI[CODELOAD]="${ZI[PLUGINS_DIR]}/_local---config"
-ZI[CODELOAD_ALIASES]="${ZI[CODELOAD]}/zsh/aliases"
 
 if [[ $AF_OFF != 1 ]]; then
   fpath=("${Plugins[CODELOAD_REPO_DIR]}/functions" "${fpath[@]}")
