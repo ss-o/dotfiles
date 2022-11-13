cd9de3350bd4ca200d7db23a4384880d389093b9
Salvydas Lukosius <team@zshell.dev>
2022-11-13 11:29:05 +0000

    Delete z-shell-org.zsh
    Signed-off-by: Salvydas Lukosius <team@zshell.dev>

--- .codeload/_local---config/zsh/aliases/z-shell-org.zsh
+++
@@ -1,6 +0,0 @@
-# CONFIG FILES
-#
-# Fetch common repository files.
-alias add-gitattributes='command wget -O - https://raw.githubusercontent.com/alexkaratarakis/gitattributes/master/Common.gitattributes >> .gitattributes'
-alias add-editorconfig='command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/editor/editorconfig/.editorconfig-root > .editorconfig'
-alias add-gitignore='command wget -O - https://raw.githubusercontent.com/z-shell/community/main/docs/project/workspace/git/gitignore_common >> .gitignore'
