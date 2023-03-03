0d20c641d0e6207f39fdbb5c3e37aa9fcf555b80
Salvydas Lukosius <sall@w-ss.io>
2023-03-03 02:46:36 +0000

    up/4

--- sync/zsh/snippets/grc.zsh
+++ sync/zsh/snippets/grc.zsh
@@ -1,21 +1,94 @@
-if (( ! $+commands[grc] )); then
+# -*- mode: zsh; sh-indentation: 2; indent-tabs-mode: nil; sh-basic-offset: 2; -*-
+# vim: ft=zsh sw=2 ts=2 et
+
+if [ ! -n "$TERM" ] || [ "$TERM" = dumb ] || (( ! $+commands[grc] )); then
   return 1
 fi
 
-  if (( ${#_grc_cmds[@]} == 0 )); then
-    _grc_cmds=(
-      'cat' 'tail' 'configure' 'diff' 'make' 'gcc' 'g++' 'as' 'gas' 'ps'
-      'ld' 'netstat' 'ping' 'ping6' 'traceroute' 'traceroute6' 'nmap' 'df'
-      'mount' 'mtr' 'ifconfig' 'dig' 'ip'
-     )
-  fi
+if (( ${#_grc_cmds[@]} == 0 )); then
+  _grc_cmds=(
+    'as'
+    'ant'
+    'blkid'
+    'cc'
+    'configure'
+    'curl'
+    'cvs'
+    'df'
+    'diff'
+    'dig'
+    'dnf'
+    'docker'
+    'docker-compose'
+    'docker-machine'
+    'du'
+    'env'
+    'fdisk'
+    'findmnt'
+    'free'
+    'g++'
+    'gas'
+    'gcc'
+    'getfacl'
+    'getsebool'
+    'gmake'
+    'id'
+    'ifconfig'
+    'iostat'
+    'ip'
+    'iptables'
+    'iwconfig'
+    'journalctl'
+    'kubectl'
+    'last'
+    'ldap'
+    'lolcat'
+    'ld'
+    'ls'
+    'lsattr'
+    'lsblk'
+    'lsmod'
+    'lsof'
+    'lspci'
+    'make'
+    'mount'
+    'mtr'
+    'mvn'
+    'netstat'
+    'nmap'
+    'ntpdate'
+    'php'
+    'ping'
+    'ping6'
+    'proftpd'
+    'ps'
+    'sar'
+    'semanage'
+    'sensors'
+    'showmount'
+    'sockstat'
+    'ss'
+    'stat'
+    'sysctl'
+    'systemctl'
+    'tcpdump'
+    'traceroute'
+    'traceroute6'
+    'tune2fs'
+    'ulimit'
+    'uptime'
+    'vmstat'
+    'wdiff'
+    'whois'
+  )
+fi
 
   # Wrap commands in grc
   for _grc_cmd in "${_grc_cmds[@]}"; do
-    if (( ${+commands[${_grc_cmd}]} )); then
+    if (( $+commands[$_grc_cmd] )); then
       eval "
-        function ${_grc_cmd} {
-          "grc -es --colour=auto ${_grc_cmd}" "\$@"
+        $_grc_cmd() {
+          "command grc -es --colour=auto $_grc_cmd" "\$@"
         }
       "
     fi
