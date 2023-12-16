91ed52dfa80fce30626f03c9dc478a402113f6a1
Salvydas Lukosius <ss-o@users.noreply.github.com>
2023-12-16 00:30:12 +0000

    modified:   .trunk/trunk.yaml

--- sync/linux/x86_64/zsh/snippets/user/options.zsh
+++ sync/linux/x86_64/zsh/snippets/user/options.zsh
@@ -3,9 +3,10 @@
 setopt bang_hist                # treat the '!' character, especially during expansion
 setopt extended_glob            # treat #, ~, and ^ as part of patterns for filename generation
 
-#setopt no_glob_dots             # show dotfiles in completions
-#setopt always_to_end            # when completing from the middle of a word, move the cursor to the end of the words
-#setopt auto_list                # automatically lists choices on an ambiguous completion
+setopt no_glob_dots             # "*" shouldn't match files starting with "." in completions
+setopt no_sh_word_split         # Use Zsh style word splitting
+setopt always_to_end            # when completing from the middle of a word, move the cursor to the end of the words
+setopt auto_list                # automatically lists choices on an ambiguous completion
 #setopt auto_menu                # automatically use menu completion
 #setopt auto_cd                  # cd by typing the directory name if it's not a command
 
@@ -20,21 +21,21 @@ setopt hist_verify              # show command with history expansion to the use
 setopt share_history            # share history data, turn off, if sharing history via "setopt inc_append_history" below
 setopt append_history           # allow multiple terminal sessions to all append to one zsh command history
 setopt extended_history         # record timestamp of command in HISTFILE
-#setopt inc_append_history       # ensures that commands are added to the history immediately
+setopt inc_append_history       # ensures that commands are added to the history immediately
 
 #setopt interactive_comments     # allow comments even in interactive shells
 #setopt multios                  # perform implicit tees or cats when multiple redirections are attempted
-#setopt no_beep                  # don't beep on error
+setopt no_beep                  # don't beep on error
 setopt prompt_subst              # enable parameter expansion, command substitution, and arithmetic expansion in the prompt
 #setopt transient_rprompt        # only show the rprompt on the current prompt
 
-#setopt pushd_ignore_dups        # don't push multiple copies of the same directory onto the directory stack
-#setopt auto_pushd               # make cd push the old directory onto the directory stack
+setopt pushd_ignore_dups        # don't push multiple copies of the same directory onto the directory stack
+setopt auto_pushd               # make cd push the old directory onto the directory stack
 #setopt no_pushd_minus           # make pushd work the same as Bash
-#setopt long_list_jobs           # display PID when suspending processes as well
+setopt long_list_jobs           # display PID when suspending processes as well
 setopt no_sh_word_split         # use zsh style word splitting
-#setopt unset                    # don't error out when unset parameters are used
-#setopt notify                   # report the status of backgrounds jobs immediately
-#setopt nohup                    # don't send SIGHUP to background processes when the shell exits
+setopt unset                    # don't error out when unset parameters are used
+setopt notify                   # report the status of backgrounds jobs immediately
+setopt nohup                    # don't send SIGHUP to background processes when the shell exits
 setopt ignore_eof               # prevent Ctrl-D from exiting the shell
 unsetopt rm_star_silent         # ask for confirmation for rm * and rm path/
