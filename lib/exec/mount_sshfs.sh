6b7c24c24ea73a0c476bdbdf457bb1c69e34919f
Salvydas Lukosius <ss-o@users.noreply.github.com>
2024-06-23 02:14:02 +0100

    modified:   .trunk/trunk.yaml, lib/*, sync/*

--- lib/exec/mount_sshfs.sh
+++ lib/exec/mount_sshfs.sh
@@ -1,37 +1,68 @@
 #!/usr/bin/env bash
 
-# Hosts required
-declare -a hosts=(
-  'core.internal'
-)
+# TODO:
+# sudo useradd --system --user-group -d /srv/sshfs -u 850 -s /usr/bin/nologin sshfs
+# sudo chown :sshfs /srv/sshfs
 
+# Hosts required
+#declare -a hosts=(
+#  'core.internal'
+#)
+
+declare host='core.internal'
+declare host_path='/mnt/dietpi_userdata'
 declare target_path='/srv/sshfs'
-declare mount_options='follow_symlinks,allow_other,uid=1000,gid=1000'
+declare mount_options="_netdev,rw,nosuid,allow_other,uid=$UID,gid=850,default_permissions,follow_symlinks,idmap=user"
+
+return_ok() {
+  if [[ -z ${RUN_QUIET} ]]; then
+    echo -e "Status: ✓"
+  fi
+  return 0
+}
+
+return_err() {
+  if [[ -z ${RUN_QUIET} ]]; then
+    echo -e "Status: ✗"
+  fi
+  clear_mount
+  return 1
+}
+
+prepare_mount() {
+  if [[ ! -d "${target_path}/${host}" ]]; then
+    command mkdir -p "${target_path}/${host}"
+  fi
+}
+
+clear_mount() {
+  if [[ -d "${target_path}/${host}" ]]; then
+    command rm -rf "${target_path}/${host}"
+  fi
+}
 
 sshfs_mount() {
-  [[ -n ${RUN_QUIET} ]] || echo -ne "Hosts: "
-  for host in "${hosts[@]}"; do
-    [[ -n ${RUN_QUIET} ]] || echo -ne "${host} "
-    if ping -c 1 "${host}" &>/dev/null; then
-      if [[ ! -d "${target_path}/${host}" ]]; then
-        mkdir -p "${target_path}/${host}"
-      fi
-      sshfs "${host}":/mnt/dietpi_userdata "${target_path}/${host}" -o "${mount_options}"
-      [[ -n ${RUN_QUIET} ]] || echo -e "✓ "
-      return 0
+  [[ -n ${RUN_QUIET} ]] || echo -e "Hosts: ${host}"
+  #  for host in "${hosts[@]}"; do
+  if command ping -c 1 "${host}" &>/dev/null; then
+    prepare_mount
+    if command sshfs "${host}:${host_path}" "${target_path}/${host}" -o "${mount_options}"; then
+      return_ok
     else
-      [[ -n ${RUN_QUIET} ]] || echo -e "✗ "
-      return 1
+      return_err
     fi
-  done
+  else
+    [[ -n ${RUN_QUIET} ]] || echo -e "Error: failed to ping $host"
+  fi
+  #  done
 }
 
 sshfs_umount() {
-  for host in "${hosts[@]}"; do
-    if ping -c 1 "${host}" &>/dev/null; then
-      fusermount -u "${target_path}/${host}"
-    fi
-  done
+  #  for host in "${hosts[@]}"; do
+  if ping -c 1 "${host}" &>/dev/null; then
+    fusermount -u "${target_path}/${host}" && clear_mount
+  fi
+  #  done
 }
 
 main() {
